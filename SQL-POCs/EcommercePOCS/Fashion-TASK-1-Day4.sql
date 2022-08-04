ae96aadd98f3fe54eb3f5e6a1b488b2e0bfcbf54
BHAVNACORP\Dhanasheela.Thula <dhanasheela.thula@bhavnacorp.com>
2022-08-04 18:29:07 +0530

    task-2 day-4 commited

--- SQL-POCs/EcommercePOCS/Fashion-TASK-1-Day4.sql
+++ SQL-POCs/EcommercePOCS/Fashion-TASK-1-Day4.sql
@@ -109,4 +109,73 @@ select * from endorse where brandname is null
 
 
 
+--triggers
 
+
+CREATE TABLE Employee
+(
+  Id int Primary Key,
+  Name nvarchar(30),
+  Salary int,
+  Gender nvarchar(10),
+  DepartmentId int
+)
+GO
+
+create table Endorse_Test
+(
+EID int primary key identity,
+ Id  INT ,
+operation varchar(30),
+updatedDate datetime ,
+) 
+-- Insert data into Employee table
+INSERT INTO Employee VALUES (1,'Pranaya', 5000, 'Male', 3)
+INSERT INTO Employee VALUES (2,'Priyanka', 5400, 'Female', 2)
+INSERT INTO Employee VALUES (3,'Anurag', 6500, 'male', 1)
+INSERT INTO Employee VALUES (4,'sambit', 4700, 'Male', 2)
+INSERT INTO Employee VALUES (9,'kumar', 6600, 'male', 2)
+
+
+--inserting values using trigger
+
+CREATE TRIGGER trInsertEmployee 
+ON Employee
+FOR INSERT 
+AS
+BEGIN
+  insert into Endorse_Test
+  select Id,'inserted',GETDATE() from inserted 
+END
+
+--updating values using trigger
+
+CREATE TRIGGER TrgUpdateEmployee 
+ON Employee
+after UPDATE 
+AS
+BEGIN
+insert into Endorse_Test
+  select Id,'updated',GETDATE() from deleted 
+    END
+
+update Employee set Name='ARden' where Id=1
+
+
+
+--deleting values using trigger
+
+CREATE TRIGGER TrgDeleteEmployee 
+ON Employee
+after DELETE 
+AS
+BEGIN
+insert into Endorse_Test
+  select Id,'deleted',GETDATE() from deleted 
+    END
+
+delete  from Employee   where Id=2
+
+
+select * from Employee
+select * from Endorse_Test
