5cb4bc7debb0c8b24b2f3ad42f7ce09b70a7fded
BHAVNACORP\Dhanasheela.Thula <dhanasheela.thula@bhavnacorp.com>
2022-08-23 17:01:35 +0530

    task-1

--- Angular-POCs/Amazon-Projec/src/app/app.component.ts
+++ Angular-POCs/Amazon-Projec/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Project-Demo';
-  
+  title = 'Amazon-Projec';
 }
--- Angular-POCs/Amazon-Projec/src/app/app.module.ts
+++ Angular-POCs/Amazon-Projec/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent,
-    HomeComponent,
-    UsersComponent,
-    ContactComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
