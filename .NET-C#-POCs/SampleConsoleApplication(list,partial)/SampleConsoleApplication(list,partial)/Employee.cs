98ebb64b254f9b9e002d6514afa52f0882e4483e
BHAVNACORP\Dhanasheela.Thula <dhanasheela.thula@bhavnacorp.com>
2022-08-29 17:34:20 +0530

    task-1

---
+++ .NET-C#-POCs/SampleConsoleApplication(list,partial)/SampleConsoleApplication(list,partial)/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApplication_list_partial_
+{
+    class Employee
+    {
+        public int empid { get; set; }
+        public string ename { get; set; }
+        public string gender { get; set; }
+        public double salary { get; set; }
+
+        //Declaring a private variable  
+        private string _company;
+        public  string company
+        {
+            get { return _company; }
+            set { _company = "BhavnaCorp"; }
+        }
+    }
+}
