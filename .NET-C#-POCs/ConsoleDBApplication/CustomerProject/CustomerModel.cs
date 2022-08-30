f98ecaa9f6a397fe993e1d2698e220b497fa33d5
BHAVNACORP\Dhanasheela.Thula <dhanasheela.thula@bhavnacorp.com>
2022-08-30 19:04:40 +0530

    task-2-day-20 Customer folder

---
+++ .NET-C#-POCs/ConsoleDBApplication/CustomerProject/CustomerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomerProject
+{
+    public class CustomerModel
+    {
+        public int customer_id { get; set; }
+        public string email { get; set; }
+        public int acco_no { get; set; }
+        public string password { get; set; }
+        public bool role { get; set; }
+    }
+}
