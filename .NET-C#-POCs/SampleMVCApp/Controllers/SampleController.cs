9bb4a90e266d2384e3507fc5eeac4164f1878c38
Dhanasheela <87488135+Dhanasheela@users.noreply.github.com>
2022-09-30 09:48:38 +0530

    SampleMVCapplication

---
+++ .NET-C#-POCs/SampleMVCApp/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleMVCWebApplication.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: SampleController
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+
+        // GET: SampleController/Details/5
+
+    }
+}
